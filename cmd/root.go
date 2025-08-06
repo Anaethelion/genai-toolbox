f4d95c724b4eaa6e60730d41d443613a74d75c4e
Laurent Saint-Félix <laurent.saintfelix@elastic.co>
2025-08-06 11:17:03 +0200

    update: add elasticsearch-list-indices tool

--- cmd/root.go
+++ cmd/root.go
@@ -56,6 +56,7 @@ import (
 	_ "github.com/googleapis/genai-toolbox/internal/tools/dgraph"
 	_ "github.com/googleapis/genai-toolbox/internal/tools/duckdbsql"
 	_ "github.com/googleapis/genai-toolbox/internal/tools/elasticsearch/elasticsearchesql"
+	_ "github.com/googleapis/genai-toolbox/internal/tools/elasticsearch/elasticsearchlistindices"
 	_ "github.com/googleapis/genai-toolbox/internal/tools/elasticsearch/elasticsearchsearch"
 	_ "github.com/googleapis/genai-toolbox/internal/tools/firestore/firestoredeletedocuments"
 	_ "github.com/googleapis/genai-toolbox/internal/tools/firestore/firestoregetdocuments"
